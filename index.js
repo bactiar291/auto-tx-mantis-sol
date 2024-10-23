7f4ae8efba759da56f51f1229038f17a2d91ef89
bactiar291 <134827499+bactiar291@users.noreply.github.com>
2024-10-23 22:20:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+console.log('\x1b[36m%s\x1b[0m', '================================');
+console.log('\x1b[32m%s\x1b[0m', '   A U T H O R :  B A C T I A R 2 9 1');
+console.log('\x1b[36m%s\x1b[0m', '================================');
+
 const { Connection, Keypair, Transaction, SystemProgram, sendAndConfirmTransaction } = require('@solana/web3.js');
 const fs = require('fs');
 
@@ -5,10 +9,8 @@ const fs = require('fs');
 const rpcUrl = 'https://mantis-testnet-rollup.composable-shared-artifacts.composablenodes.tech/rpc';
 const connection = new Connection(rpcUrl, 'confirmed');
 
-
 const secretKeyData = JSON.parse(fs.readFileSync('secretKey.json', 'utf-8'));
 const secretKeyArray = secretKeyData.secretKeyArray;
-
 
 const senderKeypair = Keypair.fromSecretKey(Uint8Array.from(secretKeyArray));
 
@@ -20,10 +22,8 @@ function getRandomWallet() {
 async function sendTransaction() {
     const recipient = getRandomWallet();
 
-    
     const amount = 0.1 * 1_000_000_000; 
 
-   
     const transaction = new Transaction().add(
         SystemProgram.transfer({
             fromPubkey: senderKeypair.publicKey,
@@ -33,17 +33,14 @@ async function sendTransaction() {
     );
 
     try {
-       
         const { blockhash } = await connection.getLatestBlockhash();
         transaction.recentBlockhash = blockhash;
         transaction.feePayer = senderKeypair.publicKey;
 
-       
         await transaction.partialSign(senderKeypair);
 
-       
         const signature = await sendAndConfirmTransaction(connection, transaction, [senderKeypair], {
-            commitment: 'confirmed', // Ensure confirmation via HTTP
+            commitment: 'confirmed',
             preflightCommitment: 'confirmed'
         });
 
@@ -54,27 +51,26 @@ async function sendTransaction() {
             signature: signature,
         };
 
-        
         fs.writeFileSync('nano.json', JSON.stringify(transactionDetails, null, 2));
 
         console.log('Transaction succeeded! Details saved to nano.json');
-        return true; 
+        return true;
 
     } catch (error) {
         console.error('Transaction failed:', error);
-        return false; 
+        return false;
     }
 }
 
 async function repeatSendTransaction() {
     while (true) {
-        const success = await sendTransaction(); 
+        const success = await sendTransaction();
 
         if (!success) {
             console.log('Retrying in 10 seconds due to error...');
-            await new Promise(resolve => setTimeout(resolve, 10000)); 
+            await new Promise(resolve => setTimeout(resolve, 10000));
         } else {
-            await new Promise(resolve => setTimeout(resolve, 5000)); 
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
     }
 }
